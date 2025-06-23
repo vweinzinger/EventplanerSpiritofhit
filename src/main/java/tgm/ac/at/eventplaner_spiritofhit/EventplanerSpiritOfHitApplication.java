12edd6a33207d2e8122a03eebebb00728effe2ff
NicoFurtner <nicofurtner@gmail.com>
2025-06-23 20:19:27 +0200

    First Commit

---
+++ src/main/java/tgm/ac/at/eventplaner_spiritofhit/EventplanerSpiritOfHitApplication.java
@@ -0,0 +1,13 @@
+package tgm.ac.at.eventplaner_spiritofhit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventplanerSpiritOfHitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventplanerSpiritOfHitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgm/ac/at/eventplaner_spiritofhit/EventplanerSpiritOfHitApplicationTests.java
@@ -0,0 +1,13 @@
+package tgm.ac.at.eventplaner_spiritofhit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventplanerSpiritOfHitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
