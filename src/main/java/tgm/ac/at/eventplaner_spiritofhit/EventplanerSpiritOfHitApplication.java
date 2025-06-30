446785e6d1a0e6d799e4f3bbfcc2a52b01fd4a15
Timon~ <thollaus@student.tgm.ac.at>
2025-06-30 10:30:05 +0200

    Changes by Nico IO (Schema.SQL)

--- src/main/java/tgm/ac/at/eventplaner_spiritofhit/EventplanerSpiritOfHitApplication.java
+++ src/main/java/tgm/ac/at/eventplaner_spiritofhit/EventplanerSpiritOfHitApplication.java
@@ -2,6 +2,7 @@ package tgm.ac.at.eventplaner_spiritofhit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.engine.IterationStatusVar;
 
 @SpringBootApplication
 public class EventplanerSpiritOfHitApplication {
